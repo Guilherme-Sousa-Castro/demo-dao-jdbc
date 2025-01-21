4c8e007554513ca0815e2e55216de15cb4b4d866
Guilherme Castro <guilherme.castro@rtc.pt>
2025-01-21 09:54:18 +0000

    Implementation findAll

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -90,7 +90,49 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public List<Seller> findAll() {
 
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName \r\n"
+					+ "FROM seller INNER JOIN department \r\n"
+					+ "ON seller.DepartmentId = department.Id\r\n"
+					+ "ORDER BY Name");
+
+			
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<Seller>();
+			Map<Integer, Department> map = new HashMap<>(); 
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instantienteDeparment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+							
+				Seller obj = instantienteSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+
+			// nao fecha a conexao pois pode ser usada novamente nos restantes metodos, é
+			// fechada depois na main
+
+		}
 	}
 
 	@Override
